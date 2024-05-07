8c50719b3b607f23a23b566e194918dd7ca08f5c
Mechy <mechfarias@gmail.com>
2024-05-06 21:20:41 -0300

    feat: traigo + data + mounted+ Vue
    Extraigo ciertos campos de los datos del Pokémon, como su peso, imágenes y nombre, del objeto data obtenido de la response.
    
    Función capitalize me devuelve el titulo con mayúscula.
    
    Mounted: se ejecuta después de que la instancia de Vue se haya "montado" en el DOM

--- main.js
+++ main.js
@@ -1,10 +1,53 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            verModal: false,
+            pokemonElegido: null,
+            pokemones: [],
+            historialPokemones: [] 
+        };
+    },
+methods: {
+        abrirModal() {
+            this.verModal = true;
+        },
+        cerrarModal() {
+            this.verModal = false;
+        },
+        async verPokemon(url) {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                const { weight, sprites, name } = data;
+                const { front_default } = sprites.other['official-artwork'];
 
-fetch("https://pokeapi.co/api/v2/pokemon/?limit=100&offset=0")
-.then(response => response.json())
-.then(data => {
-    this.pokemones = data.results;
-    console.log('Datos obtenidos:', data);
-})
-.catch(error => {
-    console.error('Error al obtener la lista de Pokémon:', error);
+                this.pokemonElegido = { weight, front_default, name};
+                this.abrirModal();
+            } catch (error) {
+                console.error('Error al obtener información del Pokémon:', error);
+            }
+        },
+        capitalize(value) {
+            if (!value) return '';
+            return value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
+        },
+    },
+
+
+    mounted() { fetch("https://pokeapi.co/api/v2/pokemon/?limit=100&offset=0")
+        .then(response => response.json())
+        .then(data => {
+            this.pokemones = data.results;
+            console.log('Datos obtenidos:', data);
+        })
+        .catch(error => {
+            console.error('Error al obtener la lista de Pokémon:', error);
+        });
+}
+
+
 });
+
+app.mount('#app');
+
+
